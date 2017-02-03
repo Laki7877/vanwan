6b329ad95d68d61f548c06377428b1f132156b21
Laki Sik <mr.laki.sik@gmail.com>
2017-02-04 01:19:15 +0700

    Init spring boot application

--- src/main/java/com/vanwan/VanwanApplication.java
+++ src/main/java/com/vanwan/VanwanApplication.java
@@ -1,9 +1,11 @@
 package com.vanwan;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class VanwanApplication {
 
 	public static void main(String[] args) {
