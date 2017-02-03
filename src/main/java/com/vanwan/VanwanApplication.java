51d562a17fbd9012a01e73d3f72b4f059f916d0e
Laki Sik <laki@th.ibm.com>
2017-02-03 23:27:29 +0700

    Initialize app

---
+++ src/main/java/com/vanwan/VanwanApplication.java
@@ -0,0 +1,12 @@
+package com.vanwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanwanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanwanApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vanwan/VanwanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vanwan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VanwanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
